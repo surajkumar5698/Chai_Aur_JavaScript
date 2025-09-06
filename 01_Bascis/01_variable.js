e39a6f7e7248d9725c23b338c3ac82d1498364df
suraj kumar <118158025+surajkumar5698@users.noreply.github.com>
2025-09-06 06:09:02 +0000

    variable

---
+++ 01_Bascis/01_variable.js
@@ -0,0 +1,27 @@
+const accountId =14263;
+let accountEmail ="suraj@gmail.com";
+var accountPassword ="14253";
+accountCity ="Noida";
+
+let accountState;  // this will give as undefine  no value is assine
+
+//accountId =45856;  // not allowed
+console.log(accountId);
+
+/*
+    prefer not to use var 
+    because of issue in block and functional scop ;
+
+*/
+
+accountEmail ="suri@gmail.com";
+accountPassword ="abcf45";
+accountCity ="Bihar";
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
