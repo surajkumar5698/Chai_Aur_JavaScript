5ced82ffa45a78b698fad229bb9042a0e59ed6a4
suraj kumar <118158025+surajkumar5698@users.noreply.github.com>
2025-09-12 13:27:14 +0000

    Data Tpye is done

---
+++ 01_Bascis/02_datatypes.js
@@ -0,0 +1,17 @@
+let name ="suraj";
+let age= 18;
+let isloggedIn =false;
+
+// number=> 2 to power 53
+//bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+//undefined => vlaue is not assign 
+//symbol  => unique
+
+// object 
+
+console.log(typeof null)
+
+// null typeof is object
