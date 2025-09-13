324d13f516c7af440f225d4a091fc9aad1bb8f1e
suraj kumar <118158025+surajkumar5698@users.noreply.github.com>
2025-09-13 05:07:42 +0000

    memorry

---
+++ 01_Bascis/memorry.js
@@ -0,0 +1,19 @@
+let myYoutubeName = "surajkumar.com";
+let anotherName =myYoutubeName;
+
+anotherName ="jai shree ram";
+console.log(anotherName);
+console.log(myYoutubeName);
+
+let userOne ={
+    email:"suraj@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email="kumar@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
